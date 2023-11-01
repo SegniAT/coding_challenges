47d9fc61a52d83040488eab8bdf3abbd35b27e5e
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-11-01 22:30:09 +0300

    Best time to buy and sell stock II done using Decreasing Monotonic Queue

--- Nov 1 2023/Best Time to Buy and Sell Stock II/main.go
+++ Nov 1 2023/Best Time to Buy and Sell Stock II/main.go
@@ -6,8 +6,17 @@ import (
 )
 
 func main() {
+	input := []int{7, 1, 5, 3, 6, 4}
+	mq := createMonotonicQueue(len(input))
+	profit := 0
 
-	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+	for i := len(input) - 1; i > -1; i-- {
+		item := Item{
+			index: i,
+			value: input[i],
+		}
+		profit += mq.push(item)
+	}
 }
 
 func maxProfit(prices []int) int {
@@ -18,4 +27,70 @@ func maxProfit(prices []int) int {
 	}
 
 	return int(profit)
+}
+
+type Item struct {
+	value int
+	index int
+}
+
+type MonotonicQueue struct {
+	queue []Item
+}
+
+func createMonotonicQueue(size int) *MonotonicQueue {
+	mq := MonotonicQueue{}
+	mq.queue = make([]Item, size)
+	return &mq
+}
+
+func (mq *MonotonicQueue) isEmpty() bool {
+	return len(mq.queue) == 0
+}
+
+func (mq *MonotonicQueue) getLast() Item {
+	length := len(mq.queue)
+	if length < 0 {
+		panic("getLast() on empty array")
+	}
+	return mq.queue[length-1]
+}
+
+func (mq *MonotonicQueue) removeLast() {
+	length := len(mq.queue)
+	if length < 0 {
+		panic("removeFirst() on empty array")
+	}
+	mq.queue = mq.queue[:length-1]
+}
+
+func (mq *MonotonicQueue) push(item Item) int {
+	profit := 0
+
+	for !mq.isEmpty() && mq.getLast().value < item.value {
+		mq.removeLast()
+	}
+
+	if !mq.isEmpty() && mq.getLast().index-item.index == 1 {
+		profit += mq.getLast().value - item.value
+	}
+
+	mq.queue = append(mq.queue, item)
+	fmt.Println("QUEUE: ", mq.queue)
+
+	return profit
+}
+
+func maxProfitMQ(prices []int) int {
+	mq := createMonotonicQueue(len(prices))
+	profit := 0
+
+	for i := len(prices) - 1; i > -1; i-- {
+		item := Item{
+			index: i,
+			value: prices[i],
+		}
+		profit += mq.push(item)
+	}
+	return profit
 }
