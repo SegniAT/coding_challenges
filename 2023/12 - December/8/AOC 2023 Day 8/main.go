f94231e21793f6e7139cc4b351360f4bc11d9ff6
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-12-08 19:58:51 +0300

    Day 8 Part II doneeee

--- 2023/12 - December/8/AOC 2023 Day 8/main.go
+++ 2023/12 - December/8/AOC 2023 Day 8/main.go
@@ -21,7 +21,6 @@ func main() {
 	dirs := input[0]
 	nodeDirs := make(map[string][]string)
 
-	fmt.Println(dirs)
 	for i := 2; i < len(input); i++ {
 		line := input[i]
 		equalsInd, openingParenthesisInd := strings.Index(line, "="), strings.Index(line, "(")
@@ -33,33 +32,71 @@ func main() {
 		nodeDirs[node] = dirs
 	}
 
-	dirsCount := len(dirs)
+	startingNodes := []string{}
+	for node := range nodeDirs {
+		if node[2] == 'A' {
+			startingNodes = append(startingNodes, node)
+		}
+	}
+
+	steps := make([]int, len(startingNodes))
+
+	for i := 0; i < len(startingNodes); i++ {
+		steps[i] = findSteps(dirs, startingNodes[i], nodeDirs)
+	}
+
+	fmt.Println("Starting nodes: ", startingNodes)
+	fmt.Println("Steps: ", steps)
+	fmt.Println("LCM: ", lcmMultiple(steps))
+	// 10668805667831
+
+}
+
+func findSteps(dirs, startNode string, nodeDirs map[string][]string) int {
+	currNode := startNode
 	i := 0
-	currNode := "AAA"
 	steps := 0
 	for true {
-		dir := dirs[i]
-
-		if dir == LEFT {
+		if dirs[i] == LEFT {
 			currNode = nodeDirs[currNode][0]
 		}
 
-		if dir == RIGHT {
+		if dirs[i] == RIGHT {
 			currNode = nodeDirs[currNode][1]
 		}
 
 		steps++
-		if currNode == "ZZZ" {
+
+		if currNode[2] == 'Z' {
 			break
 		}
+
 		i++
-		if i >= dirsCount {
+		if i >= len(dirs) {
 			i = 0
 		}
 	}
 
-	fmt.Println("Steps: ", steps)
+	return steps
+}
 
+// Function to calculate the GCD (Greatest Common Divisor)
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+// Function to calculate the LCM (Least Common Multiple) of multiple numbers
+func lcmMultiple(numbers []int) int {
+	lcm := numbers[0]
+
+	for i := 1; i < len(numbers); i++ {
+		lcm = (lcm * numbers[i]) / gcd(lcm, numbers[i])
+	}
+
+	return lcm
 }
 
 func readInput(src string) ([]string, error) {
