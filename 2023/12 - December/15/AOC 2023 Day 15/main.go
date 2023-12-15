0de98085b16a5bff546f48ac617b72c5ab31cd13
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-12-15 13:21:00 +0300

    Day 15 Part 1 done

---
+++ 2023/12 - December/15/AOC 2023 Day 15/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	input, err := loadInput("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Input: ", input)
+
+	sum := 0
+
+	for _, inp := range input {
+		sum += getHash(inp)
+	}
+
+	fmt.Println("SUM: ", sum)
+
+	// PART I
+	// Attempt 1: 513158 (DONE!)
+
+	// PART II
+}
+
+func getHash(str string) int {
+	value := 0
+
+	for _, char := range str {
+		value += int(char)
+		value *= 17
+		value %= 256
+	}
+
+	return value
+}
+
+func loadInput(src string) ([]string, error) {
+	file, err := os.Open(src)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+	out := []string{}
+
+	scanner.Scan()
+	line := strings.Split(scanner.Text(), ",")
+
+	for _, l := range line {
+		out = append(out, strings.TrimSpace(l))
+	}
+
+	return out, nil
+}
